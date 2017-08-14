c0b48dd96f53b7477427eefb76a3af7748a240b1
rohanhg91 <rohanhgupta91@gmail.com>
2017-08-14 16:42:16 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,17 @@
 def solution(dic):
     """Enter Code Here"""
+    i = 0
+    f = []
+
+    while i < len(dic):
+        field, value = dic.items()[i]
+        f.append(field)
+        i = i + 1
+    return f
+
+
+
+'''
+a = solution({1: 12, 2: 23, 5: 34,8: 90})
+print a
+'''
